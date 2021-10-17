998f4208f0507ef01233578511dca6838739f0b4
Uiliam Bomfim <wf3@outlook.com.br>
2021-10-17 17:26:01 -0300

    add commit

--- src/enums/ProductTypes.java
+++ src/enums/ProductTypes.java
@@ -1,31 +1,8 @@
 package enums;
 
-import model.Book;
-import model.Product;
-
 public enum ProductTypes {	
 	
-	BOOK {
-		@Override
-		public String getCaract() {
-
-			return Book.getIsbn();
-		}
-	}, 
-	DISCIPLINA {
-		@Override
-		public String getCaract() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-	}, 
-	COURSE {
-		@Override
-		public String getCaract() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-	};
-	
-	public abstract String getCaract();
+	BOOK,
+	DISCIPLINA,
+	COURSE;
 }
