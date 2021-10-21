de13005f7b7ce28aab10616776329ddd6c510121
amirbrito <amir.sbrito@gmail.com>
2021-10-21 00:23:17 -0300

    refatorando class model ementa

--- src/model/Ementa.java
+++ src/model/Ementa.java
@@ -1,41 +1,27 @@
 package model;
 
-
 public class Ementa {
 	
-	Course course;
-
-	
+	Course course;	
 	
 	public Ementa(Course course) {
 		super();
 		this.course = course;
-
 	}
-
-
-
+	
 	public Course getCourse() {
 		return course;
 	}
-
-
-
+	
 	public void setCourse(Course course) {
 		this.course = course;
-	}
-	
-	
-		
+	}		
 	
 	public String toString() {
 		return "Ementa: " + course.getName()+"\n"+
-				"Código do Curso: " + course.hashCode() +"\n"+
+				"Cï¿½digo do Curso: " + course.hashCode() +"\n"+
 				"Livros: " + course.getBooks()+"\n"+
 				"Disciplinas: " + course.getClasses(); 
 	}
 	
-	
-	
-
 }
